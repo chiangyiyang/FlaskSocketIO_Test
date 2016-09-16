3deb635b5b525f3f752ebd5c266ee922ee13cc56
Yiyang <chiangyiyang@gmail.com>
2016-09-16 15:57:40 +0800

    init commit

---
+++ app.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+
+from flask import Flask, render_template
+from flask_socketio import SocketIO
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!123456'
+socketio = SocketIO(app)
+
+@app.route("/")
+def hello():
+    return "Hello World! 456"
+
+@app.route("/ws")
+def hellows():
+    return render_template('index.html')
+
+
+@socketio.on('my event')
+def handle_my_custom_event(json):
+    print('received json: ' + str(json))
+
+if __name__ == '__main__':
+    socketio.run(app)
